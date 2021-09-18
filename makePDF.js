13b05a683f923408dc2884113013e1c87206fae2
Mahdi Moqaddami <mah_d10@outlook.com>
2021-09-18 21:34:37 +0430

    Add try catch to puppeteer to stop unhandled promise rejecction error.

--- makePDF.js
+++ makePDF.js
@@ -1,12 +1,25 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 exports.makePDF = (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://varzesh3.com', {
-    waitUntil: 'networkidle2',
-  });
-  await page.pdf({ path: 'v3-a4-header.pdf', format: 'a4', margin:{top: 20}, printBackground: true, displayHeaderFooter:true, headerTemplate: '<span class="pageNumber"></span>'});
-  // console.log("hello");
-  await browser.close();
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto("https://varzesh3.com", {
+      waitUntil: "networkidle2",
+    });
+    await page.pdf({
+      path: "v3-a4-header.pdf",
+      format: "a4",
+      margin: { top: 20 },
+      printBackground: true,
+      displayHeaderFooter: true,
+      headerTemplate: '<span class="pageNumber"></span>',
+    });
+    // console.log("hello");
+    await browser.close();
+  } catch (e) {
+    console.log(e);
+  }
 })();
